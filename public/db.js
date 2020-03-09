9056f0832631dfaa3d5be8854b5eaa2bf29a2de9
JT Turner <j.tturner14@yahoo.com>
2020-03-09 19:45:25 -0400

    corrected typo in db.js

--- public/db.js
+++ public/db.js
@@ -2,7 +2,7 @@ let db;
 //create a new database named budget
 const request = indexedDB.open("budget", 1);
 
-request.onupgradeneeded = request(event) {
+request.onupgradeneeded = function(event) {
     const db = event.target.result;
     db.createObjectStore("pending", {autoIncrement: true});
 };
