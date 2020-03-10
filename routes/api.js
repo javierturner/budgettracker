8ee3509eabcc785973d1ea1876e1e587de9621e0
JT Turner <j.tturner14@yahoo.com>
2020-03-10 17:35:28 -0400

    post routes

--- routes/api.js
+++ routes/api.js
@@ -8,3 +8,13 @@ router.post("/api/transaction", ({body}, res) => {
         res.status(400).json(err);
     });
 });
+
+router.post("/api/transaction/bulk", ({body}, res) => {
+    Transaction.insertMany(body)
+    .then(dbTransaction => {
+        res.json(dbTransaction);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+
