be7e649ad224fbf082e123d3d62925eb5b9e37e6
JT Turner <j.tturner14@yahoo.com>
2020-03-10 17:32:36 -0400

    routes

---
+++ routes/api.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const Transaction = require("../models/transaction");
+
+router.post("/api/transaction", ({body}, res) => {
+    Transaction.create(body).then(dbTransaction => {
+        res.json(dbTransaction);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
