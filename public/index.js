dbf422751adc45e90b46430dff3d73fed6eecb24
JT Turner <j.tturner14@yahoo.com>
2020-03-09 20:36:06 -0400

    finished populate chart function

--- public/index.js
+++ public/index.js
@@ -40,3 +40,42 @@ function populateTable() {
         tbody.appendChild(tr);
     });
 };
+
+function populateChart() {
+    //copy the array and reverse
+    const reversed = transactions.slice().reverse();
+    let sum = 0;
+
+    //creating dates for chart
+    const labels = reversed.map(t => {
+        const date = new Date(t.date);
+        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+    });
+
+    const data = reversed(t => {
+        sum += parseInt(t.value);
+        return sum;
+    });
+
+    if (myChart) {
+        myChart.destroy();
+    }
+
+    const ctx = document.getElementById("myChart").getContext("2d");
+
+    myChart = new myChart(ctx, {
+        type: "line",
+        date: {
+            labels,
+            datasets: [
+                {
+                    label: "Total over time",
+                    fill: true,
+                    backgroundColor: "#6666ff",
+                    data
+                }
+            ]
+        }
+    })
+}
+
