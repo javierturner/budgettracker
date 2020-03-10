b13a65d14845aadb09ded2fccb3ea95ee5510a56
JT Turner <j.tturner14@yahoo.com>
2020-03-10 17:28:18 -0400

    service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -19,5 +19,41 @@ self.addEventListener("install", function(evt) {
 })
 
 self.addEventListener("activate", function(evt) {
-    
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cached data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
 })
+
+
+//fetch
+self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request).then(response => {
+                    if (response.status === 200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(evt.request);
+                });
+            }).catch(err => console.log(err))
+        );
+        return;
+    }
+    evt.respondWith(caches.match(evt.request).then(function(response) {
+        return response || fetch(evt.request);
+    }));
+});
