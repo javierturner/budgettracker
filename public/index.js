8648ebc41080f54060293d6716308a42b2af6e0b
JT Turner <j.tturner14@yahoo.com>
2020-03-11 13:28:53 -0400

    fixed error in table

--- public/index.js
+++ public/index.js
@@ -82,33 +82,37 @@ function populateChart() {
 function sendTransaction(isAdding) {
     const nameEl = document.querySelector("#transactionName");
     const amountEl = document.querySelector("#transactionAmount");
-    const errorEl = document.querySelector("#form .error");
+    const errorEl = document.querySelector("form .error");
 
+    // validate form
     if (nameEl.value === "" || amountEl.value === "") {
-        errorEl.textContent = "You are missing information";
+        errorEl.textContent = "Missing Information";
         return;
     } else {
-        errorEl.textContent = " ";
+        errorEl.textContent = "";
     }
 
-
+    // create record
     const transaction = {
         name: nameEl.value,
         value: amountEl.value,
         date: new Date().toISOString()
     };
 
+    // if subtracting funds, convert amount to negative number
     if (!isAdding) {
         transaction.value *= -1;
     }
 
-    //add to the beginning of the current data array
+    // add to beginning of current array of data
     transactions.unshift(transaction);
 
+    // re-run logic to populate ui with new record
     populateChart();
     populateTable();
     populateTotal();
 
+    // also send to server
     fetch("/api/transaction", {
         method: "POST",
         body: JSON.stringify(transaction),
@@ -116,21 +120,80 @@ function sendTransaction(isAdding) {
             Accept: "application/json, text/plain, */*",
             "Content-Type": "application/json"
         }
-    }).then(response => response.json())
+    })
+        .then(response => response.json())
         .then(data => {
             if (data.errors) {
-                errorEl.textContent = "You are missing information";
+                errorEl.textContent = "Missing Information";
             } else {
+                // clear form
                 nameEl.value = "";
                 amountEl.value = "";
             }
-        }).catch(err => {
+        })
+        .catch(err => {
+            // fetch failed, so save in indexed db
             saveRecord(transaction);
 
+            // clear form
             nameEl.value = "";
             amountEl.value = "";
         });
 }
+
+
+// function sendTransaction(isAdding) {
+//     const nameEl = document.querySelector("#transactionName");
+//     const amountEl = document.querySelector("#transactionAmount");
+//     const errorEl = document.querySelector("#form .error");
+
+//     if (nameEl.value === "" || amountEl.value === "") {
+//         errorEl.textContent = "You are missing information";
+//         return;
+//     } else {
+//         errorEl.textContent = "";
+//     }
+
+
+//     const transaction = {
+//         name: nameEl.value,
+//         value: amountEl.value,
+//         date: new Date().toISOString()
+//     };
+
+//     if (!isAdding) {
+//         transaction.value *= -1;
+//     }
+
+//     //add to the beginning of the current data array
+//     transactions.unshift(transaction);
+
+//     populateChart();
+//     populateTable();
+//     populateTotal();
+
+//     fetch("/api/transaction", {
+//         method: "POST",
+//         body: JSON.stringify(transaction),
+//         headers: {
+//             Accept: "application/json, text/plain, */*",
+//             "Content-Type": "application/json"
+//         }
+//     }).then(response => response.json())
+//         .then(data => {
+//             if (data.errors) {
+//                 errorEl.textContent = "You are missing information";
+//             } else {
+//                 nameEl.value = "";
+//                 amountEl.value = "";
+//             }
+//         }).catch(err => {
+//             saveRecord(transaction);
+
+//             nameEl.value = "";
+//             amountEl.value = "";
+//         });
+// }
 
 document.querySelector("#addBtn").addEventListener("click", function (event) {
     event.preventDefault();
